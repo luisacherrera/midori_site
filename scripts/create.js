923b4a0600b6572624f5cafab1514919b10eac36
luisacedomrf <luis.acedo@marfeel.com>
2020-07-01 19:11:01 +0200

    simplify html

---
+++ scripts/create.js
@@ -0,0 +1,23 @@
+import photos from '../statics/photosHome';
+
+const list = document.getElementById('carousel');
+
+function buildElements(name, path) {
+    let item = document.createElement('li');
+    item.className = 'carousel-item';
+    item.innerHTML = `
+            <div class="carousel-item_container">
+                <a href="" class="carousel-item_container_link">
+                    <div class="text-container">
+                        <div class="text-container_center">
+                            <p class="carousel-item_container_description">${name}</p>
+                        </div>
+                    </div>    
+                    <img src="${path}" alt="${name}" class="carousel-item_container_image">
+                </a>
+            </div>`
+    
+    list.appendChild(item);
+}
+
+photos.photos.forEach(photo => buildElements(photo.name, photo.file));
---
+++ statics/photosHome.js
@@ -0,0 +1,84 @@
+const photos = {
+    photos: [
+        {
+            "name": "Concurs Biblioteca d'Arenys de Mar (finalista)",
+            "file": "statics/Concurs Biblioteca d'Arenys de Mar (finalista).jpg"
+        },
+        {
+            "name": "Concurs Biblioteca d'Arenys de Mar (finalista)",
+            "file": "statics/Concurs Biblioteca d'Arenys de Mar (finalista)(2).jpg"
+        },
+        {
+            "name": "Exposició Clarisses- passat, present i futur (2018)",
+            "file": "statics/Exposició Clarisses- passat, present i futur (2018)(1).jpg"
+        },
+        {
+            "name": "Exposició Clarisses- passat, present i futur (2018)",
+            "file": "statics/Exposició Clarisses-passat, present i futur (2018)(2).jpg"
+        },
+        {
+            "name": "Festival A Cel Obert 2018",
+            "file": "statics/Festival A Cel Obert 2018(1).jpg"
+        },
+        {
+            "name": "Festival A Cel Obert 2018",
+            "file": "statics/Festival A Cel Obert 2018(2).jpg"
+        },
+        {
+            "name": "Festival Lluèrnia 2016",
+            "file": "statics/Festival Lluèrnia 2016(1).jpg"
+        },
+        {
+            "name": "Festival Lluèrnia 2016",
+            "file": "statics/Festival Lluèrnia 2016(2).jpg"
+        },
+        {
+            "name": "Festival Perpetracions 2018",
+            "file": "statics/Festival Perpetracions 2018(1).jpg"
+        },
+        {
+            "name": "Festival Perpetracions 2018",
+            "file": "statics/Festival Perpetracions 2018(2).jpg"
+        },
+        {
+            "name": "Festival Tudanzas 2015",
+            "file": "statics/Festival Tudanzas 2015.jpg"
+        },
+        {
+            "name": "Prototip Showroom modular",
+            "file": "statics/Prototip Showroom modular (1).jpg"
+        },
+        {
+            "name": "Prototip Showroom modular",
+            "file": "statics/Prototip Showroom modular (2).jpg"
+        },
+        {
+            "name": "Reforma Malats (en curs)",
+            "file": "statics/Reforma Malats (en curs).jpg"
+        },
+        {
+            "name": "Reforma Sant Adrià (en curs)",
+            "file": "statics/Reforma Sant Adrià (en curs).jpg"
+        },
+        {
+            "name": "Stands Espai Ciència (Saló de l'Ensenyament 2018)",
+            "file": "statics/Stands Espai Ciència (Saló de l'Ensenyament 2018)(1).jpg"
+        },
+        {
+            "name": "Stands Espai Ciència (Saló de l'Ensenyament 2018)",
+            "file": "statics/Stands Espai Ciència (Saló de l'Ensenyament 2018)(2).jpg"
+        },
+        {
+            "name": "Stands Espai Ciència (Saló de l'Ensenyament 2019)",
+            "file": "statics/Stands Espai Ciència (Saló de l'Ensenyament 2019).jpg"
+        },
+        {
+            "name": "Stands Firantoni 2015",
+            "file": "statics/Stands Firantoni 2015(1).jpg"
+        },
+        {
+            "name": "Stands Firantoni 2015",
+            "file": "statics/Stands Firantoni 2015(2).jpg"
+        }
+    ]
+}
