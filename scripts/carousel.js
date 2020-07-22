c4905f7becb50d214e7a43579c9738a32772816f
luisacedomrf <luis.acedo@marfeel.com>
2020-07-22 11:47:32 +0200

    no touch listener

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -3,7 +3,6 @@ const items = document.getElementsByClassName('carousel-item');
 
 let currentItem; 
 
-window.addEventListener('touchmove', manualScroll);
 window.addEventListener('resize', checkOrientation);
 
 const scroll = setTimeout(scroller, 5000);
